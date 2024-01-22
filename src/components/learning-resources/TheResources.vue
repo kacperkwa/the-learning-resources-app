ee9faed1d62d292d63111feda043170a0397d3cd
kacperkwa <kacperkwartaxz@gmail.com>
2024-01-22 18:04:57 +0100

    added some templates

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseCard>
+    <BaseButton @click="setSelectedTab('stored-resources')"
+      >Stored Resources</BaseButton
+    >
+    <BaseButton @click="setSelectedTab('add-resource')"
+      >Add Resource</BaseButton
+    >
+  </BaseCard>
+  <component :is="selectedTab"></component>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let selectedTab = ref('stored-resources');
+const setSelectedTab = (tab) => {
+  selectedTab = tab;
+};
+</script>
